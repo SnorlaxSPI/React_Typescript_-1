4b50d2f92f088ab0a24f9a849ab7f1e70e6ad8d9
aflinsjr@icloud.com <aflinsjr@icloud.com>
2023-04-15 13:28:42 -0300

    3.3 - Listando repositórios

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,10 +1,12 @@
 function RepositoryItem(props: any) {
   return (
     <li>
-    <strong>{props.repository.name ?? 'Default'}</strong>
-    <p>{props.repository.description}</p>
-    <a href='https://github.com/unform/unform'>Acessar repositório</a>
-  </li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+
+      <a href = {props.repository.html_url}>
+        Acessar repositório</a>
+    </li>
   );
 }
 
